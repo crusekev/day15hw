609c207143a452366f4468bbe703283a7a02440e
crusekev <crusekev@gmail.com>
2018-03-08 09:12:55 -0600

    //Day15hw and day 16 dg //changed for day16hw (incomplete) //day17dg changes

--- src/kevincruse/Main.java
+++ src/kevincruse/Main.java
@@ -9,6 +9,7 @@ public class Main {
         final int COLUMNS=5;
         double[][]scores=new double[ROWS][COLUMNS];
         String[]names=new String[ROWS];
+        double[]medianarray=new double[11]; //will only be one column
         int position=0;
         int c=0;
         int r=0;
@@ -52,17 +53,6 @@ public class Main {
             }
             scores[11][c]=scores[11][c]/11.0;
         }
-        for(r=1;r<=11;r++) {
-            System.out.printf("%-20s",names[r]);
-            for(c=1;c<=4;c++) {
-                //possibly make the averages double here
-                if(r==11&&c==4) {
-                    break;
-                }
-                System.out.printf("%10.0f",scores[r][c]);
-            }
-            System.out.println();
-        }
         //column 4 should be double bc its average and the last row as well
         double lowest=301.0;
         String lowname="";
@@ -82,6 +72,71 @@ public class Main {
                 highname=names[r];
             }
         }
-        System.out.printf("%.2f was made by %2s \n",highest,highname);
+        System.out.printf("%.2f was made by %2s\n",highest,highname);
+        bubblesort(names,11,scores,11);
+        System.out.println("\n\n");
+        for(r=1;r<=11;r++) {
+            System.out.printf("%-20s",names[r]);
+            for(c=1;c<=4;c++) {
+                //possibly make the averages double here
+                if(r==11&&c==4) {
+                    break;
+                }
+                System.out.printf("%10.0f",scores[r][c]);
+            }
+            System.out.println();
+        }
+        //calc median
+        for(r=1;r<=10;r++) {
+            medianarray[r]=scores[r][4];
+            System.out.println(medianarray[r]);
+        }
+        Arrays.sort(medianarray);
+        System.out.println("\n\nmedian array sorted");
+        for(r=1;r<=10;r++) {
+            System.out.println(medianarray[r]);
+        }
+    }
+    public static void bubblesort(String[]names,int length,double[][]scores,int length2) {
+        int i;
+        int j;
+        int z;
+        String temp;
+        double itemp;
+        boolean test;
+        for(i=length-1;i>0;i--) {
+            test=true;
+            for(j=1;j<i;j++) {
+                if(names[j].compareToIgnoreCase(names[j+1])>0) {
+                    temp=names[j];
+                    names[j]=names[j+1];
+                    names[j+1]=temp;
+                    for(z=1;z<5;z++) {
+                        itemp=scores[j][z];
+                        scores[j][z]=scores[j+1][z];
+                        scores[j+1][z]=itemp;
+                    }
+                    test=false;
+                }
+            }
+            if(test) {
+                break;
+            }
+        }
     }
 }
+/* code for median
+
+if(size%2==0) {
+                        //even number of elements
+                        mid1=size/2;
+                        mid2=(size/2)-1;
+                        med=(array[mid1]+array[mid2])/2.0;
+                        System.out.println("Median: "+med);
+                    }
+                    else {
+                        //odd number of elements
+                        med=array[size/2];
+                        System.out.println("Median: "+med);
+                    }
+*/
